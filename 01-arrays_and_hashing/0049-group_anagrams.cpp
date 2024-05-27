0873c9891fce64b475d9ded3de6630d23ed39304
Bailey Faulk <90534590+yardboy27@users.noreply.github.com>
2024-05-27 16:51:05 -0500

    Create 0049-group_anagrams.cpp

---
+++ 01-arrays_and_hashing/0049-group_anagrams.cpp
@@ -0,0 +1,46 @@
+/* 49. Group Anagrams
+ * Link: https://leetcode.com/problems/group-anagrams/description/
+ */
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+        // this map will have a sorted word as the key
+        // and the value is a vector of the strs that
+        // equal that key when they are sorted
+
+        // we can sort using non-comparison based sort --> counting sort
+        for (string _str : strs)
+        {
+            // create a vector that represents the alphabet
+            vector<int> count(26, 0);
+            // loop through word and add to count for frequency of letters
+            for (char _char : _str)
+                ++count[_char - 'a'];
+
+            // now reconstruct the sorted word
+            string key;
+            for (int i = 0; i < count.size(); ++i)
+            {
+                for (int j = 0; j < count.at(i); ++j)
+                    key += char(int('a') + i);
+                
+                // std::cout << i << key << std::endl; // debug
+            }
+
+            // now add this word to the unordered map
+            map[key].push_back(_str);
+        }
+
+        // now create the answer
+        vector<vector<string>> answer;
+        for ( auto& pair : map )
+        {
+            answer.push_back(pair.second);
+        }
+
+        return answer;
+        
+    }
+};
